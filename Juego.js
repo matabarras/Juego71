2adef1871f6b08e99259d79b4f8c2b81f5211c88
Luis C <armixluis@gmail.com>
2021-01-18 00:05:55 +0100

    Añado Readme y pequeña corrección

--- Juego.js
+++ Juego.js
@@ -11,13 +11,14 @@ let sumacartas = 0;
 
 do {
     primeracarta = damecarta();
-    console.log(primeracarta);
     while(primeracarta == 8 || primeracarta == 9){
        
         primeracarta = damecarta();
        
         
     }
+
+    console.log("La carta obtenida es " + primeracarta);
     
     if (primeracarta == 10 || primeracarta == 11 || primeracarta == 12) {
         valorcarta = 0.5;
@@ -25,15 +26,16 @@ do {
         valorcarta = primeracarta;
     }
 
-    console.log("El valor de la  carta es: " + valorcarta);
-     pregunta = rl.question("Quieres otra carta?");
-    pregunta = pregunta.toUpperCase();
     sumacartas += valorcarta;
-    console.log("El valor acumulado es "+ sumacartas);
+    console.log("Tu valor acumulado es : " + sumacartas);
+    pregunta = rl.question("Quieres otra carta?");
+    pregunta = pregunta.toUpperCase();
+    
+    
     if(sumacartas> 7.5){
         console.log("No puedo darte mas cartas, porque tu valor es superior a 7.5");
     }
 
     
 }
-while (pregunta == "SI" && sumacartas < 7.5) 
+while (pregunta != "NO" && sumacartas <= 7.5) 
---
+++ Readme.txt
@@ -0,0 +1,10 @@
+Juego basico.
+
+Se genera cartas mientras el jugador quiera.
+
+Reglas:
+
+Si la suma de las cartas generadas es  mayor de 7,5 se acaba el juego.
+Si no quiere mas cartas se acaba el juego.
+Si las cartas son 10,11,12 el valor sera 0,5 si nos sera el mismo valor que la carta generada
+Si la carta es 8 o 9 no sirve, debe generar otra estas  no juegan.
